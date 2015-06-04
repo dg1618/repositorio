b50835dd062d7994c70a84724e5cacd39f88cc8b
diegoporras <diegojazzsports@gmail.com>
2015-06-03 21:12:28 -0600

    base

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yoman1App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the yoman1App
+ */
+angular.module('yoman1App')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yoman1App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yoman1App
+ */
+angular.module('yoman1App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
